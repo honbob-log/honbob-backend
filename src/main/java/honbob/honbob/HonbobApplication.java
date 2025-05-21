90a3c94b0952d4eb382b0b0dd5bb29cfdacf8388
Juhye0k <kyoung1678@naver.com>
2025-05-21 14:13:56 +0900

    feat:초기 프로젝트 세팅

---
+++ src/main/java/honbob/honbob/HonbobApplication.java
@@ -0,0 +1,13 @@
+package honbob.honbob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HonbobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HonbobApplication.class, args);
+	}
+
+}
