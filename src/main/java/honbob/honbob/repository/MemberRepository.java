94edc770c5b72c0ff818ba7233be1ec562996c29
Juhye0k <kyoung1678@naver.com>
2025-05-30 14:44:29 +0900

    feat : 카카오 OAuth Repository #5

---
+++ src/main/java/honbob/honbob/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package honbob.honbob.repository;
+
+import honbob.honbob.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByKakaoId(Long kakaoId);
+    Optional<Member> findByEmail(String email);
+    Optional<Member> findByRefreshToken(String refreshToken);
+}
