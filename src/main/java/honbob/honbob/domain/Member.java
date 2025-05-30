3c3d7559474c05876f55ce26e3e03f50768e37df
Juhye0k <kyoung1678@naver.com>
2025-05-30 14:45:26 +0900

    refactor : Member 수정 #5

--- src/main/java/honbob/honbob/domain/Member.java
+++ src/main/java/honbob/honbob/domain/Member.java
@@ -3,11 +3,13 @@ package honbob.honbob.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 public class Member {
 
@@ -17,14 +19,21 @@ public class Member {
     @Column(nullable = false)
     private String name;
 
-    @Column(nullable = false)
+
     private String email;
 
-    @Column(nullable = false)
+    @Column
     private String password;
 
-    @Column(nullable = false)
+
     private String profileImage;
+
+    // 카카오 관련 필드 추가
+    @Column(unique = true)
+    private Long kakaoId;
+
+    @Column
+    private String refreshToken;
 
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
     private List<Recipe> recipeList;
@@ -37,4 +46,16 @@ public class Member {
 
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
     private List<UserIngredient> userIngredientList;
+
+    // 카카오 회원 생성 메서드
+    public static Member createKakaoMember(Long kakaoId, String name, String email, String profileImage, String refreshToken) {
+        Member member = new Member();
+        member.setKakaoId(kakaoId);
+        member.setName(name);
+        member.setEmail(email);
+        member.setProfileImage(profileImage);
+        member.setRefreshToken(refreshToken);
+        member.setPassword(""); // 소셜 로그인은 비밀번호가 없음
+        return member;
+    }
 }
