429c86daba30ec9d478483172bee9d2835cc7e0c
Juhye0k <kyoung1678@naver.com>
2025-05-30 14:42:05 +0900

    feat : 카카오 OAuth dto #5

---
+++ src/main/java/honbob/honbob/dto/KakaoTokenDto.java
@@ -0,0 +1,25 @@
+package honbob.honbob.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class KakaoTokenDto {
+    
+    @JsonProperty("access_token")
+    private String accessToken;
+    
+    @JsonProperty("token_type")
+    private String tokenType;
+    
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+    
+    @JsonProperty("expires_in")
+    private Integer expiresIn;
+    
+    @JsonProperty("refresh_token_expires_in")
+    private Integer refreshTokenExpiresIn;
+}
