106c69546d0b513fa6290a826c73be75c389cc15
Juhye0k <kyoung1678@naver.com>
2025-06-01 20:27:33 +0900

    feat : 유저 정보 등록 Controller

---
+++ src/main/java/honbob/honbob/controller/GlobalLoginController.java
@@ -0,0 +1,27 @@
+package honbob.honbob.controller;
+
+import honbob.honbob.dto.UserProfileRequest;
+import honbob.honbob.global.response.ResponseBody;
+import honbob.honbob.global.response.ResponseUtil;
+import honbob.honbob.service.member.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class GlobalLoginController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/api/auth/login/profile")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<ResponseBody<String>> setProfile(@RequestBody UserProfileRequest userProfileRequest, @AuthenticationPrincipal Long memberId){
+        memberService.setMemberInfo(userProfileRequest, memberId);
+        return ResponseEntity.ok(ResponseUtil.createSuccessResponse("유저 정보 설정 완료"));
+    }
+}
